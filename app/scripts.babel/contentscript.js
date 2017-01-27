ba398d4f41b0ef33bc2624575bd490339b38045b
Hugo Bastien <hugobast@gmail.com>
2017-01-27 14:30:06 -0500

    Display errors

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -4,6 +4,10 @@ console.log('\'Allo \'Allo! Content script');
 
 chrome.runtime.onMessage.addListener(function(message, _, response) {
   if (message.action === "verify") {
+    if (!document.querySelectorAll('textarea')) {
+      return
+    }
+
     let content = document.querySelectorAll('textarea')[0].value;
 
     response({ content: content });
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -2,10 +2,47 @@
 
 chrome.extension.getBackgroundPage().console.log('still works');
 
+function templated(match) {
+  var value = "Aucune suggestions"
+
+  if (match.replacements.length > 0) {
+    value = match.replacements[0].value
+  }
+
+  return `
+    <li>
+      <dl>
+        <dt>Règle:</dt>
+        <dd>${match.rule.description}</dd>
+
+        <dt>Correction</dt>
+        <dd>${value}</dd>
+      </dl>
+    </li>
+  `
+}
+
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   chrome.tabs.getSelected(null, function(tab) {
-    chrome.tabs.sendMessage(tab.id, { action: "verify" }, function(content) {
-      console.log(content);
+    chrome.tabs.sendMessage(tab.id, { action: "verify" }, function(textarea) {
+      console.log(textarea);
+
+      let instance = axios.create({
+        baseURL: 'https://secret-forest-62803.herokuapp.com'
+      });
+
+      instance.get('/v2/check', { params: { language: 'fr', text: textarea.content } })
+        .then((response) => {
+          chrome.browserAction.setBadgeText({ text: response.data.matches.length.toString() });
+
+          response.data.matches.forEach((match, index) => {
+            console.log(match);
+
+            $(templated(match)).appendTo('.matches');
+          });
+
+          console.log(response);
+        });
     });
   });
 });
